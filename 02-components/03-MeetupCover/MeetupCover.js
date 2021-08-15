ef3d2e493515e055115644f9eaaec0701e345828
Alexander Kulyk <akulyk@yandex.ua>
2021-08-15 17:40:26 +0300

    Task 02-components/03-MeetupCover done

--- 02-components/03-MeetupCover/MeetupCover.js
+++ 02-components/03-MeetupCover/MeetupCover.js
@@ -1,10 +1,24 @@
-import { defineComponent } from './vendor/vue.esm-browser.js';
+import {defineComponent} from './vendor/vue.esm-browser.js';
 
 export default defineComponent({
   name: 'MeetupCover',
-
+  props: {
+    title: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+  },
+  computed: {
+    style() {
+      const style = {};
+      if (this.image) style['--bg-url'] = `url('${this.image}')`;
+      return style;
+    },
+  },
   template: `
-    <div class="meetup-cover" style="--bg-url: url('https://course-vue.javascript.ru/api/images/2')">
-        <h1 class="meetup-cover__title">Title</h1>
+    <div class="meetup-cover" :style="style">
+    <h1 v-if="title" class="meetup-cover__title">{{title}}</h1>
     </div>`,
 });

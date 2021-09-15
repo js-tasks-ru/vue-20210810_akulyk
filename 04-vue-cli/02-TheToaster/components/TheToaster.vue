a40257a3fea542393105b1afae0af5286b56f243
Alexander Kulyk <akulyk@yandex.ua>
2021-09-15 22:48:03 +0300

    Task 04-vue-cli/02-TheToaster done

--- 04-vue-cli/02-TheToaster/components/TheToaster.vue
+++ 04-vue-cli/02-TheToaster/components/TheToaster.vue
@@ -1,24 +1,59 @@
 <template>
-  <div class="toasts">
-    <div class="toast toast_success">
-      <ui-icon class="toast__icon" icon="check-circle" />
-      <span>Success Toast Example</span>
-    </div>
-
-    <div class="toast toast_error">
-      <ui-icon class="toast__icon" icon="alert-circle" />
-      <span>Error Toast Example</span>
+  <div v-if="messages.length" class="toasts">
+    <div v-for="message in messages" :key="message.id" class="toast" :class="['toast_' + message.type]">
+      <ui-icon v-if="message.icon" class="toast__icon" :icon="message.icon" />
+      <span>{{ message.text }}</span>
+      <span v-if="message.closable" class="close">
+        <ui-icon class="toast__icon" icon="trash" @click="remove(message.id)" />
+      </span>
     </div>
   </div>
 </template>
 
 <script>
 import UiIcon from './UiIcon';
+let id = 0;
+const iconMap = {
+  success: 'check-circle',
+  error: 'alert-circle',
+};
 
 export default {
   name: 'TheToaster',
-
   components: { UiIcon },
+  data() {
+    return {
+      messages: [],
+    };
+  },
+  methods: {
+    toast(text, type, options = {}) {
+      const icon = iconMap[type];
+      const message = {
+        id: id++,
+        text,
+        type,
+        icon,
+        timeout: options.timeout !== undefined ? options.timeout : 5000,
+        closable: options.closable !== undefined ? options.closable : true,
+      };
+      this.messages.push(message);
+      if (message.timeout) {
+        setTimeout(() => {
+          this.remove(message.id);
+        }, message.timeout);
+      }
+    },
+    remove(messageId) {
+      this.messages = this.messages.filter((m) => m.id !== messageId);
+    },
+    success(text, options = {}) {
+      this.toast(text, 'success', options);
+    },
+    error(text, options = {}) {
+      this.toast(text, 'error', options);
+    },
+  },
 };
 </script>
 
@@ -32,6 +67,10 @@ export default {
   justify-content: flex-end;
   white-space: pre-wrap;
   z-index: 999;
+}
+
+.close {
+  cursor: pointer;
 }
 
 @media all and (min-width: 992px) {
@@ -69,5 +108,13 @@ export default {
 
 .toast.toast_error {
   color: var(--red);
+}
+
+.toast.toast_warning {
+  color: var(--orange);
+}
+
+.toast.toast_info {
+  color: var(--blue);
 }
 </style>
